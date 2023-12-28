3b1ff66a090fb5cad1c22ad6c3bb48bd5bf198c7
lozhu <zhulongkun20@icloud.com>
2023-12-29 00:47:28 +0800

    add Dockerfile

---
+++ carryyou-backend/docker_build_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "🛠️ start work flow!"
+
+echo "🧹 package cleaning.."
+mvn clean
+
+echo "📦 package application.."
+mvn package -DskipTests
+
+echo "🗑️ removing local image.."
+docker rmi chenxii81/carryyou-backend:1.0.0
+
+echo "📦 building docker image.."
+docker build -t chenxii81/carryyou-backend:1.0.0 .
+
+echo "🎉 all works finished!"
---
+++ carryyou-backend/docker_start_container.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "👷 start carryyou-backend docker container.."
+
+echo "🗑️ remove container if exists.."
+docker rm carryyou-backend
+
+echo "🧑‍💻 start application.."
+docker run -dp 8000:8000 --name=carryyou-backend -v /home/longkun/downloads/yolo:/data -e dbURL=jdbc:postgresql://localhost:5432/db_carryyou -e dbUsername=postgres -e dbPassword=secret -e dbDriver=org.postgresql.Driver -e tokenExpireAfterMinutes=10 chenxii81/carryyou-backend:1.0.0
+
+echo "🎉 carryyou-backend is running now!"
